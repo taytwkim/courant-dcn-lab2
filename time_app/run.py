27d360e61b63cefe97d6390997387c7793219e2e
김태완 <tay.tw.kim@gmail.com>
2025-02-21 18:05:18 -0500

    lab2 submission

--- time_app/run.py
+++ time_app/run.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import datetime
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
+@app.route('/time')
+def get_time():
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 app.run(host='0.0.0.0',
         port=8080,
